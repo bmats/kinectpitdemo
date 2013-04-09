9ea9d1293fb2ce5239c7b5f6f61143d0d0b5691e
Bryce <bmats@users.noreply.github.com>
2013-04-09 16:36:55 -0700

    Decreasing SW rendering quality and adding documentation.

--- Rotate3D/SolidWorks.cs
+++ Rotate3D/SolidWorks.cs
@@ -8,6 +8,9 @@ using SldWorks;
 using SwConst;
 
 namespace Rotate3D {
+    /// <summary>
+    /// Interfaces with SolidWorks using the COM API.
+    /// </summary>
     class SolidWorks {
         private static ISldWorks app = new SldWorks.SldWorks();
 
@@ -15,16 +18,17 @@ namespace Rotate3D {
         private ModelDocExtension extension;
         private ModelView view;
 
+        /// <summary>
+        /// Creates a new SolidWorks connector to the currently active document in SolidWorks.
+        /// </summary>
         public SolidWorks() {
             try {
                 this.modelDoc  = (ModelDoc2)app.ActiveDoc;
                 this.extension = this.modelDoc.Extension;
                 this.view      = (ModelView)this.modelDoc.ActiveView;
 
-                //app.SetUserPreferenceToggle((int)swUserPreferenceToggle_e.swPerformanceRemoveDetailDuringZoomPanRotate, true);
-                //app.SetUserPreferenceIntegerValue((int)swUserPreferenceToggle_e.swPerformanceRemoveDetailDuringZoomPanRotate, true);
-                //this.view.DisplayMode = (int)swViewDisplayMode_e.swViewDisplayMode_Faceted;
-                //app.SetUserPreferenceIntegerValue((int)swUserPreferenceIntegerValue_e.swEdgesTangentEdgeDisplay, (int)swEdgesTangentEdgeDisplay_e.swEdgesTangentEdgeDisplayRemoved);
+                // Set lower quality for quicker rendering
+                this.view.DisplayMode = (int)swViewDisplayMode_e.swViewDisplayMode_Shaded;
             }
             catch (Exception e) {
                 MessageBox.Show("Error connecting to SolidWorks:\r\n" + e, "Kinect Demo", MessageBoxButton.OK, MessageBoxImage.Error);
@@ -32,32 +36,48 @@ namespace Rotate3D {
             }
         }
 
+        /// <summary>
+        /// Call before gripping, to pause SolidWorks dynamics.
+        /// </summary>
         public void PreRender() {
             this.view.StopDynamics();
-            //this.modelDoc.SetTessellationQuality(0);
-            //this.modelDoc.GraphicsRedraw2();
         }
 
+        /// <summary>
+        /// Call after gripping, to restart SolidWorks dynamics.
+        /// </summary>
         public void PostRender() {
             this.view.StartDynamics();
         }
 
+        /// <summary>
+        /// Zoom the SolidWorks view to fit.
+        /// </summary>
         public void ZoomToFit() {
             this.modelDoc.ViewZoomtofit2();
         }
 
+        /// <summary>
+        /// Rotate the camera around the center of the model by the specified angles.
+        /// </summary>
+        /// <param name="xAngle">The relative horizontal rotation, in radians.</param>
+        /// <param name="yAngle">The relative vertical rotation, in radians.</param>
         public void AdjustViewAngle(double xAngle, double yAngle) {
-            // flip y for input
+            // Flip y for input
             yAngle *= -1;
 
             this.view.RotateAboutCenter(yAngle, xAngle);
         }
 
+        /// <summary>
+        /// Zoom the camera by the specified amount.
+        /// </summary>
+        /// <param name="zDist">The zoom factor (1.0 is no zooming).</param>
         public void AdjustZoom(double zDist) {
-            // invert z for input
+            // Invert z for input
             zDist *= -1;
 
-            // convert to a factor
+            // Convert to a factor relative to 1.0
             zDist += 1;
 
             this.view.ZoomByFactor(zDist);
